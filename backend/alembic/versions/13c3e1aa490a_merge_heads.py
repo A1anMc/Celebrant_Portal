215ffa9c07d32a03481b6e7c99dcc22d5d93ca1d
Alan McCarthy <alanmccarthy00@gmail.com>
2025-08-30 20:09:14 +1000

    Fix migration conflict - merge heads and stamp current state

---
+++ backend/alembic/versions/13c3e1aa490a_merge_heads.py
@@ -0,0 +1,24 @@
+"""merge_heads
+
+Revision ID: 13c3e1aa490a
+Revises: 8457b0a1acbf
+Create Date: 2025-08-30 20:08:22.609634
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '13c3e1aa490a'
+down_revision = '8457b0a1acbf'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
